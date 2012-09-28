30183c1be6f7927001792ed42fe12ba863f6c311
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-28 14:55:02 +0000

    modified print methods and so on to use median instead of average

--- trunk/src/main/java/ex1/Avoider2.java
+++ trunk/src/main/java/ex1/Avoider2.java
@@ -15,11 +15,11 @@ public class Avoider2 extends AbstractNodeMain {
 
     public static final String DASHES = "---------------------------------";//Debugging
 
-    public static final double MAX_SPEED = 0.5f;
+    public static final double MAX_SPEED = 0.6f;
     public static final float SAFE_DISTANCE = 0.5f; // In metres
     public static final int SECTORS_CHECKED = 21; // Check this many sectors
     public static final double EPSILON = 0.05;
-    public static final double DEFAULT_ROTATION = 0.087266389; // 5 degrees
+    public static final double DEFAULT_ROTATION = 0.3f; // about 10 degrees
     public static final float DEFAULT_REPLACEMENT_FOR_ZERO = 7.0f;
     public static final float IGNORE_THRESHOLD = 0.01f;
 
@@ -44,21 +44,22 @@ public class Avoider2 extends AbstractNodeMain {
                 System.out.println(scan.getRanges().length);
 
                 float[][] readings = LaserUtil.getSectors(SECTORS_CHECKED, scan);
-                float[] avgs = LaserUtil.medianOfEachSector(readings);
-                float minAvg = LaserUtil.minReading(avgs, DEFAULT_REPLACEMENT_FOR_ZERO, IGNORE_THRESHOLD);
+                float[] medians = LaserUtil.medianOfEachSector(readings);
+                float minMedian = LaserUtil.minReading(medians, DEFAULT_REPLACEMENT_FOR_ZERO, IGNORE_THRESHOLD);
 
-                System.out.println("Averages: ");
-                for (int i= 0; i < avgs.length; i++) {
-                    System.out.println(avgs[i]);
+                System.out.println("Medians: ");
+                for (int i= 0; i < medians.length; i++) {
+                    System.out.println(medians[i]);
                 }
                 System.out.println("");
 
-                System.out.println("MinAvg: " + minAvg);
-                if (minAvg > SAFE_DISTANCE + MAX_SPEED) {
-                    System.out.println("SAFE max");
+                System.out.println("MinMedian: " + minMedian);
+                if (minMedian > SAFE_DISTANCE + MAX_SPEED) {
+                    System.out.println("Safe distance: minMedian exceeds " + SAFE_DISTANCE + MAX_SPEED);
                     moveForward(MAX_SPEED);
                 } else {
-                    System.out.println("Rotating: "+DEFAULT_ROTATION);
+                    System.out.println("UNSAFE: minMedian is less than " + (SAFE_DISTANCE + MAX_SPEED) + ". Rotating: " + DEFAULT_ROTATION);
+
                     //moveForward(avg - SAFE_DISTANCE);
                     rotate(DEFAULT_ROTATION);
                 }
