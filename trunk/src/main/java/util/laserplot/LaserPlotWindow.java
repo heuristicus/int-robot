f442c9d4fe40a347a016d262299eb36f5df4e97e
Horatio Caine <hxcaine@gmail.com>
2012-09-28 15:21:10 +0000

    Added a VisualLaserPlot feature to allow us to see what the robot sees. Untested yet (need to commit to get it onto Penny).

---
+++ trunk/src/main/java/util/laserplot/LaserPlotWindow.java
@@ -0,0 +1,23 @@
+package util.laserplot;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class LaserPlotWindow extends JFrame {
+
+	public static final int WINDOW_OFFSET_X = 100;
+	public static final int WINDOW_OFFSET_Y = 100;
+
+//	public static final int WINDOW_SIZE_X = 512;
+//	public static final int WINDOW_SIZE_Y = 512;
+	
+	public LaserPlotWindow(JPanel panel, int windowSizeX, int windowSizeY) {
+		setTitle("LaserPlotWindow");
+        setSize(windowSizeX, windowSizeY);
+        setBounds(WINDOW_OFFSET_X, WINDOW_OFFSET_Y, windowSizeX, windowSizeY);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panel);
+	}
+}
---
+++ trunk/src/main/java/util/laserplot/VisualLaserPlot.java
@@ -0,0 +1,85 @@
+package util.laserplot;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Subscriber;
+
+import sensor_msgs.LaserScan;
+
+public class VisualLaserPlot extends AbstractNodeMain {
+
+	public static final Color BACKGROUND_COLOUR = Color.black;
+	
+	// The range of the laser (so we know where to plot up to 
+	public static final float RANGE_MIN = 0.02f;
+	public static final float RANGE_MAX = 5.60f;
+	
+	// Regular points plotted on the chart
+	public static final int PLOT_POINT_WIDTH = 2;
+	public static final int PLOT_POINT_HEIGHT = 2;
+	public static final Color PLOT_POINT_COLOR = Color.white;
+	
+	// If we get a zero we plot it differently for emphasis
+	public static final int ZERO_POINT_WIDTH = 5;
+	public static final int ZERO_POINT_HEIGHT = 5;
+	public static final Color ZERO_POINT_COLOR = Color.red;
+	
+	JPanel panel;
+	float[] latestReadings = new float[512];
+	
+	// Size of the window/panel
+	public int sizeX = latestReadings.length;
+    public int sizeY = (int)(RANGE_MAX * 100.0f);
+	
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("VisualLaserPlot");
+	}
+
+    @Override
+    public void onStart(ConnectedNode node) {
+    	panel = new LaserPlotPanel();
+    	panel.setBackground(BACKGROUND_COLOUR);
+    	
+        Subscriber<LaserScan> laser = node.newSubscriber("base_scan", LaserScan._TYPE);
+
+        laser.addMessageListener(new MessageListener<LaserScan>() {
+            @Override
+            public void onNewMessage(LaserScan laser) {
+            	latestReadings = laser.getRanges();
+            	panel.repaint();
+            }
+        });
+        
+        LaserPlotWindow window = new LaserPlotWindow(panel, sizeX, sizeY);
+        window.setVisible(true);
+    }
+    
+    @SuppressWarnings("serial")
+	public class LaserPlotPanel extends JPanel {
+    	@Override
+    	public void paint(Graphics g) {
+    		g.clearRect(0, 0, sizeX, sizeY);
+    		g.setColor(PLOT_POINT_COLOR);
+    		float reading;
+    		
+    		for (int i = 0; i < latestReadings.length; i++) {
+                reading = latestReadings[i];
+    			if (reading != 0) {
+    				g.drawOval(i, (int)(reading * 100), PLOT_POINT_WIDTH, PLOT_POINT_HEIGHT);
+                } else { // We draw zeros differently
+                	g.setColor(ZERO_POINT_COLOR);
+                	g.drawOval(i, (int)(reading * 100), ZERO_POINT_WIDTH, ZERO_POINT_HEIGHT);
+                	g.setColor(PLOT_POINT_COLOR);
+                }
+            }
+    	}
+    }
+}
