d86911b56272ab134da83ffa66956aed3fde6722
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-15 21:41:34 +0000

    Added a prototype stratified sampling method.

--- trunk/src/main/java/ex2/SamplingMethods.java
+++ trunk/src/main/java/ex2/SamplingMethods.java
@@ -46,9 +46,72 @@ public class SamplingMethods {
         return newPoses;
     }
 
+    public static ArrayList<Pose> lowVarianceSample(List<Pose> poseList, double[] weights, double totalWeight, int numberOfSamples, Random randGen){
+        // Equation source: Probabilistic Robotics (Thrun 2005)
+        // M = totalNumOfWeights
+        // m = 1 to M (iteration counter)
+        // Get a random number 'r' between 0 and M^-1
+        // s = r
+        // On every iteration, s += M^-1
+        // If s >= totalWeightSoFar
+        //     then pick the particle with index curParticle
+        //     else loop through weights, accumulating total until s >= totalWeightSoFar
+
+        final int M = numberOfSamples;
+        final double stepSize = totalWeight / M;
+        final double r = randGen.nextDouble() * stepSize;
+        double s = r;
+
+        double totalWeightSoFar = 0.0;
+        int curParticle = 0;
+
+        ArrayList<Pose> newPoses = new ArrayList<Pose>(M);
+        for (int m = 0; m < M; m++) {
+            while (s > totalWeightSoFar) {
+                totalWeightSoFar += weights[curParticle];
+                curParticle++;
+            }
+            Pose nextPose = poseList.get(curParticle);
+            newPoses.add(nextPose);
+            s += stepSize;
+        }
+
+        if (newPoses.size() != poseList.size()) {
+            System.out.println("WARNINGS! ALL OF THE WARNINGS! LOW VARIANCE "
+                    + "SAMPLING MUST BE BROKEN. NEW POSES: "+newPoses.size()+
+                    " AND POSELIST: "+poseList.size());
+        }
+
+        return newPoses;
+    }
+
     public static ArrayList<Pose> stratifiedSample(List<Pose> poseList, double[] weights, double totalWeight, Random randGen){
-        System.out.println("not implemented yet");
-        return null;
+	// boundary between two groups
+	int boundary = weights.length/2;
+	double groupOneTotal = 0.0;
+	double groupTwoTotal = 0.0;
+	
+	// calculate weight distribution in the two groups
+	for (int i = 0; i < weights.length; i++){
+	    if (i < boundary){
+		groupOneTotal += weights[i];
+	    } else {
+		groupTwoTotal += weights[i];
+	    }
+	}
+	
+	// Give the groups a proportional weight between 0 and 1
+	double groupOneWeight = groupOneTotal / totalweight;
+	double groupTwoWeight = groupTwoTotal / totalweight;
+		
+	int groupOneParticleNumber = groupOneWeight * weights.length;
+	int groupTwoParticleNumber = weights.length - groupOneParticleNumber;
+	
+	ArrayList<Pose> newPoses = new ArrayList<Pose>(lowVarianceSample(poseList, weights, totalWeight, groupOneParticleNumber, Random randGen));
+	newPoses.addAll(lowVarianceSample(poseList, weights, totalWeight, groupTwoParticleNumber, Random randGen));
+	
+
+        return newPoses;
     }
 
     public static ArrayList<Pose> stackSample(List<Pose> poseList, double[] weights, double totalWeight, Random randGen){
