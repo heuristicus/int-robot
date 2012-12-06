cb90f44bdc3e248f47eab12c62c4b86b93922cbd
Hamzah Salem <hxs830@cs.bham.ac.uk>
2012-12-06 06:46:32 +0000

    GUI made to ask person if they wish to go to meeting room or not

---
+++ trunk/src/main/java/ex4/DialogBox.java
@@ -0,0 +1,87 @@
+package ex4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class DialogBox extends JDialog implements ActionListener, Runnable {
+
+    enum response {
+
+        YES, NO, NORESPONSE
+    }
+    private JButton jButton_Yes = null;
+    private JButton jButton_No = null;
+    private response userResponse = response.NORESPONSE;
+    private Thread thread = null;
+    private int seconds = 0;
+    private final int max = 15;//max number of seconds 
+    private String title;
+
+    public DialogBox(String title, String message) {
+        setModal(true);
+
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        this.title = title;
+        setTitle(title);
+        jButton_Yes = new JButton("Yes");
+        jButton_Yes.addActionListener(this);
+        
+        jButton_No = new JButton("No");
+        jButton_No.addActionListener(this);
+
+        JLabel jLabel = new JLabel(message);
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        Font font = new Font("Dialog", Font.PLAIN, 24);
+        jLabel.setFont(font);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        Container cont = getContentPane();
+        //cont.setLayout();
+        cont.add(jLabel, BorderLayout.CENTER);
+        buttonPanel.add(jButton_Yes);
+        buttonPanel.add(jButton_No);
+        cont.add(buttonPanel, BorderLayout.SOUTH);
+        pack();
+        thread = new Thread(this);
+        thread.start();
+        setSize(600, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jButton_Yes) {
+            userResponse = response.YES;
+        }
+        if (e.getSource() == jButton_No) {
+            userResponse = response.NO;
+        }
+        setVisible(false);
+    }
+
+    public void run() {
+        while (seconds < max) {
+            seconds++;
+            setTitle(title + " (" + (max - seconds) + "s)");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException exc) {
+            };
+        }
+        setVisible(false);
+    }
+
+    public response getUserResponse() {
+        return userResponse;
+    }
+
+    public static void main(String[] args) {//testing
+        DialogBox cd = new DialogBox("Meeting", "Would you like to attend a meeting?");
+        System.out.println(cd.getUserResponse().name());
+        cd = null;
+        System.exit(0);
+    }
+}//end
+
